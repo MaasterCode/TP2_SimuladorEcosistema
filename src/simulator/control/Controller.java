41ce709b7e740bb7ff63ad8533a583869097a216
MasterCode <felix2mil@gmail.com>
2024-02-26 10:37:08 +0100

    Creados los metodos de Controller

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -1,5 +1,21 @@
 package simulator.control;
 
+import simulator.model.Simulator;
+
 public class Controller {
+	
+	private Simulator _sim;
+	
+	public Controller(Simulator sim)	 {
+		this._sim = sim;
+	}
+	
+	public void load_data(JSONObject data) {
+		
+	}
+	
+	public void run(double t, double dt, boolean sv, OutputStream out) {
+		
+	}
 
 }
